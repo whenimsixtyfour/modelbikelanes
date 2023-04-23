e88933b3b4620d9029b6cce5fd9c1217541b7aa5
whenimsixtyfour <marreroella44@gmail.com>
2023-04-23 15:23:16 -0500

    started doc of getting data via osmnx in py

--- get_data.py
+++ get_data.py
@@ -1,3 +1,15 @@
 # city of chicago data portal bike routes data:
 # https://data.cityofchicago.org/Transportation/Bike-Routes/3w5d-sru8
 # will need to decide on data type
+
+# getting OSM data from Open Streets using osmnx
+# note: must active via conda active ox
+import osmnx as ox
+import geopandas 
+
+# get geodata for chicago and test case (lincoln square)
+lsq_name = "Lincoln Square, Chicago, IL"
+chi_name = 'Chicago, IL'
+lsq = ox.geocode_to_gdf(lsq_name)
+lsq_geom = ox.geometries_from_place(lsq)
+
